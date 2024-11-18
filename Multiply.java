d3e302d09f7a70e0d3421480e5d7b615223fd992
Kanika Vashist <vashistkanika321@gmail.com>
2024-11-18 18:09:37 +0530

    add multiply file

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+
+        System.out.println("Multiply of the 2 num " + (a*b));
+    }
+    
+}
